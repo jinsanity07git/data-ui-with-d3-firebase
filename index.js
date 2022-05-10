48588cf8e271300e81ea0ed137f37e967d441c49
jinsanity07git <zjin@ctps.org>
2022-05-10 14:18:47 -0400

    Joining Data to Multiple Elements

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const data = [
 const svg = d3.select('svg');
 
 // console.log(d3.selectAll('rect').data(data))
-svg.selectAll('rect') 
+const rect = svg.selectAll('rect') 
   .data(data)
   .attr('width', d => d.width)
   .attr('height', d => d.height)
   .attr('fill', d => d.fill);
 
 
+console.log(rect)

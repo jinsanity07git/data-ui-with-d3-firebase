2c9fbfeb63fc5c6a08ff2e782af9c3cd9917a648
jinsanity07git <zjin@ctps.org>
2022-05-10 14:26:48 -0400

    The Enter Selection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const data = [
   {width: 200, height: 100, fill: 'purple'},
   {width: 100, height: 60, fill: 'pink'},
-  {width: 50, height: 30, fill: 'red'}
+  {width: 50, height: 30, fill: 'red'},
+  {width: 30, height: 20, fill: 'green'}
 ];
 
 // select the svg conatiner first
@@ -11,13 +12,15 @@ const svg = d3.select('svg');
 const rects = svg.selectAll('rect')
   .data(data);
 
+console.log(rects) 
+
 // add attrs to rects already in the DOM
 rects.attr('width', d => d.width)
   .attr('height', d => d.height)
   .attr('fill', d => d.fill);
 
 // append the enter selection to the DOM
-const added = rects.enter()
+const added = rects.enter()  //The Enter Selection : create virtual element
   .append('rect')
     .attr('width', d => d.width)
     .attr('height', d => d.height)

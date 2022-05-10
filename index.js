b5087ea94510c971215e8d6f68dff2ee0f561ee7
jinsanity07git <zjin@ctps.org>
2022-05-10 14:02:40 -0400

    Joining Data to an Element

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const data = [
-  {width: 200, height: 100, fill: 'purple'}
+  {width: 300, height: 100, fill: 'purple'}
 ];
 
 // select the svg container first
 const svg = d3.select('svg');
 
 svg.select('rect')
-  .data(data)
-  .attr('width', function(d,i,n){ return d.width })
+  .data(data) //d for data
+  .attr('width', function(d,i,n){ 
+    console.log(i)   // index of this element
+    console.log(n)   // the current selection
+    return d.width })
   .attr('height', function(d){ return d.height })
   .attr('fill', function(d){ return d.fill });
 

624ded958d0ff4c5c8f92215a98f2ba0b3d60f17
jinsanity07git <zjin@ctps.org>
2022-05-10 10:37:39 -0400

    select DOM object

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// const a = document.querySelectorAll('div');
-// const b = d3.selectAll('div');
-// console.log(a, b)
+const a = document.querySelectorAll('div');
+const b = d3.selectAll('div');
+console.log(a)
+
+console.log(b)
 
 const canvas = d3.select('.canvas');
